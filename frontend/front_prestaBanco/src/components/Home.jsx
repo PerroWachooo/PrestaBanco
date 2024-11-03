810729eafaf5b7363e04a7a3ddaccc7581520306
Bastian B <bastian.brito.g@usach.cl>
2024-11-03 14:29:29 -0300

    microcambio en home.jsx

--- frontend/front_prestaBanco/src/components/Home.jsx
+++ frontend/front_prestaBanco/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="content">
         <h1>Bienvenido a Presta Banco</h1>
         <p></p>
-        <p>Esta aplicacion permite simular y solicitar prestamos</p>
+        <p>Esta aplicacion permite simular y solicitar prestamos Probandocambio :u</p>
       </div>
     </div>
   );
