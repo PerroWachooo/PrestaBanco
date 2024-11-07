54217842e8b444d5900ad1e03c988cdbdedd59ba
Bastian B <bastian.brito.g@usach.cl>
2024-11-07 17:10:48 -0300

    feature botones menu

--- frontend/front_prestaBanco/src/components/Sidemenu.jsx
+++ frontend/front_prestaBanco/src/components/Sidemenu.jsx
@@ -9,6 +9,11 @@ import ListItemText from "@mui/material/ListItemText";
 import HomeIcon from "@mui/icons-material/Home";
 import { useNavigate } from "react-router-dom";
 import PlusIcon from '@mui/icons-material/Add';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 export default function Sidemenu({ open, toggleDrawer }) {
   const navigate = useNavigate();
@@ -36,35 +41,35 @@ export default function Sidemenu({ open, toggleDrawer }) {
 
         <ListItemButton onClick={() => navigate("/user/list")}>
           <ListItemIcon>
-          <PlusIcon/>
+          <SupervisedUserCircleIcon/>
           </ListItemIcon>
           <ListItemText primary="Ver Usuarios" />
         </ListItemButton>
 
         <ListItemButton onClick={() => navigate("/aplication")}>
           <ListItemIcon>
-          <PlusIcon/>
+          <AccountBalanceIcon/>
           </ListItemIcon>
           <ListItemText primary="Solicitar Credito" />
         </ListItemButton>
 
         <ListItemButton onClick={() => navigate("/simulation")}>
           <ListItemIcon>
-          <PlusIcon/>
+          <AnalyticsIcon/>
           </ListItemIcon>
           <ListItemText primary="Simular credito" />
         </ListItemButton>
 
         <ListItemButton onClick={() => navigate("/aplication/list")}>
           <ListItemIcon>
-          <PlusIcon/>
+          <AttachMoneyIcon/>
           </ListItemIcon>
           <ListItemText primary="Ver solicitudes" />
         </ListItemButton>
 
         <ListItemButton onClick={() => navigate("/aplication/totalCost")}>
           <ListItemIcon>
-          <PlusIcon/>
+          <CalculateIcon/>
           </ListItemIcon>
           <ListItemText primary="Calcular Costo Total Solicitud" />
         </ListItemButton>
